67f2c9f6dde273b76c614a21f8c37e4c6365c958
edahigure <edahigure@gmail.com>
2022-12-19 22:10:00 -0600

    correct linters

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,25 +1,23 @@
-const hambuger = document.querySelector("#hambuger");
-const menu = document.querySelector("#mobile-menu");
-const closeBtn = document.querySelector("#close-btn");
+const hambuger = document.querySelector('#hambuger');
+const menu = document.querySelector('#mobile-menu');
+const closeBtn = document.querySelector('#close-btn');
 
 function display() {
-  menu.style.display = "block";
+  menu.style.display = 'block';
 }
 
 function hide() {
-    menu.style.display = "none";
-  }
+  menu.style.display = 'none';
+}
 
-  hambuger.addEventListener('click',display);
-  closeBtn.addEventListener('click',hide);
+hambuger.addEventListener('click', display);
+closeBtn.addEventListener('click', hide);
 
-  portfolio=document.querySelector("#menu-portfolio");
-  portfolio.addEventListener('click',hide);
+const portfolio = document.querySelector('#menu-portfolio');
+portfolio.addEventListener('click', hide);
 
-  contact=document.querySelector("#menu-contact");
-  contact.addEventListener('click',hide);
+const contact = document.querySelector('#menu-contact');
+contact.addEventListener('click', hide);
 
-  about=document.querySelector("#menu-about");
-  about.addEventListener('click',hide);
-
-  
+const about = document.querySelector('#menu-about');
+about.addEventListener('click', hide);
