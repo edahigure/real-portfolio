e4bedef173e4aa88d1e49cc930fdeba604db4f6e
Kender ROMAIN <kenderromain@gmail.com>
2022-12-20 16:31:28 -0400

    Add the cards dynamically

--- popup.js
+++ popup.js
@@ -18,7 +18,39 @@ let cards_info =  [
         technologies: [ 'html', 'bootstrap', 'Ruby' ],
         link_to_live_version: '#',
         link_to_source: '#'        
-    }
+    },
+    {
+        name: 'Website Portfolio', 
+        description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard", 
+        featured_image: './images/img-work-mobile.png',
+        technologies: [ 'html', 'bootstrap', 'Ruby' ],
+        link_to_live_version: '#',
+        link_to_source: '#'        
+    },
+    {
+        name: 'Profesional Art Printing Data', 
+        description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard", 
+        featured_image: './images/img-work-mobile.png',
+        technologies: [ 'html', 'bootstrap', 'Ruby' ],
+        link_to_live_version: '#',
+        link_to_source: '#'        
+    },
+    {
+        name: 'Data Dashboard Healthcare', 
+        description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard", 
+        featured_image: './images/img-work-mobile.png',
+        technologies: [ 'html', 'bootstrap', 'Ruby' ],
+        link_to_live_version: '#',
+        link_to_source: '#'        
+    },
+    {
+        name: 'Website Portfolio', 
+        description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard", 
+        featured_image: './images/img-work-mobile.png',
+        technologies: [ 'html', 'bootstrap', 'Ruby' ],
+        link_to_live_version: '#',
+        link_to_source: '#'        
+    },
 ];
 
 const card_works = document.querySelector('.card-first');
@@ -52,3 +84,33 @@ let big_string=`
 `;
 
 card_works.innerHTML = big_string; 
+
+const wrapperCards = document.querySelector('.wrapper-cards');
+let buffer_str=`<div class="card card-0">
+<a class="orange-button" href="#">
+    See Project
+</a>                                
+</div>`;
+for(let i=0;i<cards_info.length;i++){
+    let project = cards_info [i];
+    // Create a div
+    // let div = document.createElement();
+    buffer_str+=`<div class="card card-${i}">
+    <h2 class="title-post"> ${project.name}</h2>
+    <p class="supporting-text">${project.description}</p>
+
+    <ul class="button-container-float">
+       <li class="button3">${project.technologies[0]}</li>
+       <li class="button3">${project.technologies[1]}</li>
+       <li class="button3">${project.technologies[2]}</li>                                                    
+    </ul>            
+    <a class="orange-button" href="#">
+        See Project
+    </a>                        
+</div>
+
+`;
+
+}
+
+wrapperCards.innerHTML=buffer_str;
