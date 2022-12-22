a6d5892deabafc506cfaaa53956b1b5545c8e0f8
edahigure <edahigure@gmail.com>
2022-12-22 10:03:13 -0600

    Add validation event to contact-form

---
+++ form-validation.js
@@ -0,0 +1,18 @@
+const contactForm = document.querySelector("#contact-form");
+console.log('contactForm:',contactForm);
+const email = document.querySelector("#email");
+const errorMsg = document.querySelector("#error-message");
+
+
+
+
+contactForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    let emailValue = email.value;
+    console.log(emailValue);
+    if ( emailValue === emailValue.toLowerCase()){
+        contactForm.submit();
+    }else{
+        errorMsg.innerHTML = "Please, enter lower case letters only";        
+    }
+});
