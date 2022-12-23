840229c184c98013ced7e5c7125a9e3cdd0baf80
Edahi Antonio Gutierrez Reyes <42626254+edahigure@users.noreply.github.com>
2022-12-23 09:40:57 -0600

    Putt an error

--- popup.js
+++ popup.js
@@ -186,22 +186,19 @@ function makeCard(obj) {
 
 wrapperCards.innerHTML = bufferStr;
 
-let closePopup;
-
+const closePopup = document.getElementById('close-popup');
 function closePopupFunc() {
   const popupWindow = document.querySelector('#popup-window');
   popupWindow.innerHTML = '';
   popupWindow.style.display = 'none';
 }
+closePopup.addEventListener('click', closePopupFunc);
 
 const cardIntro = document.querySelector('#button-card-intro');
-
 function displayIntro() {
   const popupWindow = document.querySelector('#popup-window');
   popupWindow.innerHTML = makeCard(introObj);
-  popupWindow.style.display = 'block';
-  closePopup = document.getElementById('close-popup');
-  closePopup.addEventListener('click', closePopupFunc);
+  popupWindow.style.display = 'block';  
 }
 cardIntro.addEventListener('click', displayIntro);
 
@@ -209,9 +206,7 @@ const card0 = document.querySelector('#button-card-0');
 function displayCard0() {
   const popupWindow = document.querySelector('#popup-window');
   popupWindow.innerHTML = makeCard(cardsInfo[0]);
-  popupWindow.style.display = 'block';
-  closePopup = document.getElementById('close-popup');
-  closePopup.addEventListener('click', closePopupFunc);
+  popupWindow.style.display = 'block';  
 }
 card0.addEventListener('click', displayCard0);
 
@@ -219,9 +214,7 @@ const card1 = document.querySelector('#button-card-1');
 function displayCard1() {
   const popupWindow = document.querySelector('#popup-window');
   popupWindow.innerHTML = makeCard(cardsInfo[1]);
-  popupWindow.style.display = 'block';
-  closePopup = document.getElementById('close-popup');
-  closePopup.addEventListener('click', closePopupFunc);
+  popupWindow.style.display = 'block';  
 }
 card1.addEventListener('click', displayCard1);
 
@@ -229,9 +222,7 @@ const card2 = document.querySelector('#button-card-2');
 function displayCard2() {
   const popupWindow = document.querySelector('#popup-window');
   popupWindow.innerHTML = makeCard(cardsInfo[2]);
-  popupWindow.style.display = 'block';
-  closePopup = document.getElementById('close-popup');
-  closePopup.addEventListener('click', closePopupFunc);
+  popupWindow.style.display = 'block';  
 }
 card2.addEventListener('click', displayCard2);
 
@@ -239,9 +230,7 @@ const card3 = document.querySelector('#button-card-3');
 function displayCard3() {
   const popupWindow = document.querySelector('#popup-window');
   popupWindow.innerHTML = makeCard(cardsInfo[3]);
-  popupWindow.style.display = 'block';
-  closePopup = document.getElementById('close-popup');
-  closePopup.addEventListener('click', closePopupFunc);
+  popupWindow.style.display = 'block';  
 }
 card3.addEventListener('click', displayCard3);
 
@@ -249,19 +238,14 @@ const card4 = document.querySelector('#button-card-4');
 function displayCard4() {
   const popupWindow = document.querySelector('#popup-window');
   popupWindow.innerHTML = makeCard(cardsInfo[4]);
-  popupWindow.style.display = 'block';
-  closePopup = document.getElementById('close-popup');
-  closePopup.addEventListener('click', closePopupFunc);
+  popupWindow.style.display = 'block';  
 }
-
 card4.addEventListener('click', displayCard4);
 
 const card5 = document.querySelector('#button-card-5');
 function displayCard5() {
   const popupWindow = document.querySelector('#popup-window');
   popupWindow.innerHTML = makeCard(cardsInfo[5]);
-  popupWindow.style.display = 'block';
-  closePopup = document.getElementById('close-popup');
-  closePopup.addEventListener('click', closePopupFunc);
+  popupWindow.style.display = 'block';  
 }
 card5.addEventListener('click', displayCard5);
